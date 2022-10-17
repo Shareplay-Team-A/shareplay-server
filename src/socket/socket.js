3d51cf5c8e1b599577365f698e5039f5b2443a0d
unknown <amytang325@gmail.com>
2022-10-17 12:47:11 -0400

    adding video-update websocket channel

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -22,6 +22,11 @@ const createServer = (httpServer) => {
     client.on('test-channel', (data) => {
       console.log('received socket data from client:', JSON.stringify(data));
     });
+
+    client.on('video-update', (data) => {
+      console.log('video update data', JSON.stringify(data));
+      client.broadcast.emit('video-update-client', data);
+    });
   });
 };
 
